fded49c5f893278007222c25ba135d13e9847be0
dougUCN <douglas.kt.wong@gmail.com>
2025-01-18 15:46:39 -0600

    Adds CORSMiddleware

--- src/chronicler_backend/main.py
+++ src/chronicler_backend/main.py
@@ -1,5 +1,6 @@
 import strawberry
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
 
 from chronicler_backend.query import MyQuery
@@ -8,4 +9,13 @@ schema = strawberry.Schema(query=MyQuery)
 graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allow only frontend URL
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],  # Specify allowed methods
+    allow_headers=["Content-Type", "Authorization"],  # Specify allowed headers
+)
+
 app.include_router(graphql_app, prefix="/graphql")
