1be55b29c5de323e75c10a1152f4f485e22bc808
dougUCN <douglas.kt.wong@gmail.com>
2025-04-30 19:17:26 -0500

    Add additional descriptions to node fields

--- src/chronicler_backend/utils/constants.py
+++ src/chronicler_backend/utils/constants.py
@@ -4,6 +4,8 @@ Constant values for convenience
 Attributes:
     ROOT_DIR (Path): The root directory of the project.
     VECTOR_DIMENSION (int): The dimension of the vector embeddings.
+    TRUNCATE_DESCRIPTION_LENGTH (int): The maximum length of the description
+        before truncation for vector conversion.
 """
 
 from pathlib import Path
@@ -11,3 +13,4 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parents[3]
 
 VECTOR_DIMENSION = 384
+TRUNCATE_DESCRIPTION_LENGTH = 200
